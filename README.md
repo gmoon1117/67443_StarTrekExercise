e8c57d869597c6670ecff0ec47cc7fb02314099f
Gloria <hyojung.gm@gmail.com>
2022-10-11 20:32:43 -0400

    finished exercise

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Star Trek playground exercise
 ---
 This is a playground for an in-class exercise with functional programming in Swift.  The answers to this will be posted later.  
 
-Qapla'
+Qapla'# 67443_StarTrekExercise
--- StarTrek.playground/Contents.swift
+++ StarTrek.playground/Contents.swift
@@ -38,24 +38,29 @@ var femaleOfficers: [String] = ["kira", "dax", "tpol", "janeway", "troi", "seven
 First, a set of warm-up problems for everyone
 */
 // Q0: How many human officers are there?
-
+var humanOfficerCount = humanOfficers.count
+print(humanOfficerCount)
 
 
 
 // Q1: What percentage of humans are female?
 
+// # femaleOfficers / # humanOfficers
+// # femaleOfficers / humanOfficers.count
+
+var femHumOfficerCount = humanOfficers.filter{femaleOfficers.contains($0)}.count
+print (Float(femHumOfficerCount) / Float(humanOfficerCount))
 
 
 
 // Q2: How many first officers are human?
-
+var firstHumOfficerCount = firstOfficers.filter{humanOfficers.contains($0.key)}.count
+print (firstHumOfficerCount)
 
 
 
 // Q3: Get a list of females sorted by name
-
-
-
+print(femaleOfficers.sorted())
 
 /*:
  Given the following struct and some methods to make these objects comparable, answer the questions in the sets below
@@ -86,26 +91,25 @@ struct Officer: Comparable {
 Group 1
 */
 // Q4: Create an array of officer objects who captain starships
-
-
+let allCaptains: [Officer] = captains.map{ Officer(name: $0, ship: $1)}
+print(allCaptains.map{$0.name})
 
 
 // Q5: Alphabetize the previous list by the captain's name
-
-
-
-
+let allCaptainsAlpha = captains.map{ Officer(name: $0, ship: $1)}.sorted(by: {$0 < $1})
+print(allCaptainsAlpha.map{$0.name})
 /*:
 Group 2
 */
 // Q6: Create an array of officers serving as first officers who are also Vulcan
 
-
-
+var vulcanFirstOfficers = firstOfficers.filter{ vulcanOfficers.contains($0.key)}.map{Officer(name: $0.key, ship: $0.value)}
+print(vulcanFirstOfficers)
 
 
 // Q7: How many these Vulcan first officers have names shorter than 5 characters?
-
+var vulcanCnt = vulcanFirstOfficers.filter{$0.name.count < 5}.map{$0.name}
+print(vulcanCnt)
 
 
 
